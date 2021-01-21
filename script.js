0e0215cbe01ab47cd66dff517f80d1d9034fbea1
VadymSorokin <s.a.rock.in619@gmail.com>
2021-01-21 15:33:08 +0200

    new 21/01/21

--- script.js
+++ script.js
@@ -2,14 +2,31 @@ const inputField = document.querySelector('#input-user');
 const addButton = document.querySelector('#add-button');
 const toDoList = document.querySelector('#to-do-list');
 
-const toClearInput = () => {
+const clearInput = () => {
 	inputField.value = '';
 }
 
-const toAdd = () => {
+const addItem = () => {
 	const list = document.createElement('li');
+	list.style.backgroundColor = 'yellow';
 	toDoList.append(list);
 	list.textContent = inputField.value;
+	const statusButton = document.createElement('button');
+	statusButton.innerHTML = 'mark as complete';
+	toDoList.append(statusButton);
+	const statusChange = () => {
+		list.style.backgroundColor = 'green';
+	};
+	statusButton.addEventListener('click', statusChange);
+	const deleteButton = document.createElement('button');
+	deleteButton.innerHTML = 'delete';
+	toDoList.append(deleteButton);
+	const deleteItem = () => {
+		list.remove();
+		statusButton.remove();
+		deleteButton.remove();
+	}
+	deleteButton.addEventListener('click', deleteItem);
 }
-addButton.addEventListener('click', toAdd);
-addButton.addEventListener('click', toClearInput);
+addButton.addEventListener('click', addItem);
+addButton.addEventListener('click', clearInput);
