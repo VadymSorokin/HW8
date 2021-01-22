c06f62d966c2a90cc108956fd720e3be00d36f49
VadymSorokin <s.a.rock.in619@gmail.com>
2021-01-22 18:11:39 +0200

    last update

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const inputField = document.querySelector('#input-user');
 const addButton = document.querySelector('#add-button');
 const toDoList = document.querySelector('#to-do-list');
-
+const notice = document.querySelector('.notice');
 const clearInput = () => {
 	inputField.value = '';
 }
-
+const delEmptyList = () => {
+	if (toDoList.firstChild !== null) notice.innerHTML = 'Your list to do, lazy monkey :';
+}
+const emptyList = () => {
+	if (toDoList.firstChild === null) notice.innerHTML = 'your list is empty ';
+}
 const addItem = () => {
 	const list = document.createElement('li');
 	list.style.backgroundColor = 'yellow';
@@ -14,10 +19,10 @@ const addItem = () => {
 	const statusButton = document.createElement('button');
 	statusButton.innerHTML = 'mark as complete';
 	toDoList.append(statusButton);
-	const statusChange = () => {
+	const statusDone = () => {
 		list.style.backgroundColor = 'green';
 	};
-	statusButton.addEventListener('click', statusChange);
+	statusButton.addEventListener('click', statusDone);
 	const deleteButton = document.createElement('button');
 	deleteButton.innerHTML = 'delete';
 	toDoList.append(deleteButton);
@@ -27,6 +32,8 @@ const addItem = () => {
 		deleteButton.remove();
 	}
 	deleteButton.addEventListener('click', deleteItem);
-}
+	deleteButton.addEventListener('click', emptyList);
+	};
 addButton.addEventListener('click', addItem);
 addButton.addEventListener('click', clearInput);
+addButton.addEventListener('click', delEmptyList);
